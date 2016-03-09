46f4c23177c4f49bb783c74f67c170d42266235c
Hubert Kamienik <Hubert.Kamienik@ericpol.com>
2016-03-09 09:55:21 +0100

    1

---
+++ plik.hpp
@@ -0,0 +1,46 @@
+#ifndef PLIK_HPP_
+#define PLIK_HPP_
+#include<vector>
+#include<string>
+class Base 
+{
+  public:
+  std::string Display;
+  virtual void wypisz ()= 0;
+};
+class Klient : public Base
+{
+  public:
+  bool status;
+  bool statusSledzony;
+  bool statusSerwera;
+  void wlaczKlienta();
+  void wylaczKlienta();
+  void wypisz (); 
+};
+class Serwer : public Base
+{
+  protected:
+  Serwer ();
+  Serwer (Serwer & a);
+  Serwer (Serwer && other);
+  static Serwer * instance;
+  public:
+  bool status; 
+  std::vector <Klient *> listaKlientow;
+  void dodajKlienta (Klient &  client);
+  void wylaczSerwer ();
+  void wlaczSerwer ();
+  static Serwer * getInstance ();
+  void wypisz (); 
+};
+
+
+
+
+
+
+
+
+
+#endif
